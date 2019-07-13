53c20b31deeda502d66f95b778b6f70e771939b5
Scott Bellware <sbellware@gmail.com>
2019-07-13 10:43:53 -0500

    Void is a class

--- test/automated/void_type.rb
+++ test/automated/void_type.rb
@@ -3,11 +3,9 @@ require_relative 'automated_init'
 context "Void Type" do
   context "Invocation" do
     test "Is an error" do
-      assert proc { Mimic::Void.anything } do
+      assert proc { Mimic::Void.new.any_method } do
         raises_error? Mimic::Void::Error
       end
     end
-
-    test "Can't be initialized (new raises error)"
   end
 end
