6a5be52429adc44e4b4ce39f872290fcec67d637
Scott Bellware <sbellware@gmail.com>
2019-08-09 17:58:00 -0500

    Vestigial control is removed

--- lib/mimic/controls/mimic.rb
+++ lib/mimic/controls/mimic.rb
@@ -4,12 +4,6 @@ module Mimic
       def self.example
         ::Mimic.(Subject::Class.example)
       end
-
-      module Class
-        def self.example
-          ::Mimic::Build.(Subject::Class.example)
-        end
-      end
     end
   end
 end
