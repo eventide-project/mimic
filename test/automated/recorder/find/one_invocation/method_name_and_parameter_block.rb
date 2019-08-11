bb8d72b8453bc15e6efc1a0f50098123bd99b72d
Scott Bellware <sbellware@gmail.com>
2019-08-11 16:26:29 -0500

    Test files are organized more descriptively

---
+++ test/automated/recorder/find/one_invocation/method_name_and_parameter_block.rb
@@ -0,0 +1,56 @@
+require_relative '../../../automated_init'
+
+context "Recorder" do
+  context "Find" do
+    context "One Invocation" do
+      context "Method Name and Parameter Block" do
+        invocation = Controls::Invocation.example
+
+        context "Recorded" do
+          recorder = Controls::Recorder.example
+
+          recorder.record(invocation)
+
+          detected_invocation = recorder.invocation(invocation.method_name) do |parameter_name, parameter_value|
+            parameter_name == :some_parameter &&
+              parameter_value == 1
+          end
+
+          context "Retrieved" do
+            assert(detected_invocation == invocation)
+          end
+        end
+
+        context "Not Recorded (Method Name)" do
+          recorder = Controls::Recorder.example
+
+          recorder.record(invocation)
+
+          detected_invocation = recorder.invocation(SecureRandom.hex) do |parameter_name, parameter_value|
+            parameter_name == :some_parameter &&
+              parameter_value == 1
+          end
+
+          test "Not Retrieved" do
+            assert(detected_invocation.nil?)
+          end
+        end
+
+        context "Not Recorded (Parameter)" do
+          recorder = Controls::Recorder.example
+
+          recorder.record(invocation)
+
+          detected_invocation = recorder.invocation(invocation.method_name) do |parameter_name, parameter_value|
+            parameter_name == :some_parameter &&
+              parameter_value == SecureRandom.hex
+          end
+
+          test "Not Retrieved" do
+            assert(detected_invocation.nil?)
+          end
+        end
+      end
+    end
+  end
+end
