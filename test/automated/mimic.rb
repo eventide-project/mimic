11524f2c6a762f9ed6ac7936b23385488aae4aef
Scott Bellware <sbellware@gmail.com>
2019-08-13 00:14:00 -0500

    Superfluous test is removed

--- test/automated/mimic.rb
+++ test/automated/mimic.rb
@@ -8,20 +8,4 @@ context "Mimic" do
   test "Constructs the mimicked class" do
     assert(mimic.is_a?(subject_class))
   end
-
-## TODO obsolete as of the recorder implementation
-## This is only one case (of two) now
-  context "Implemented Methods" do
-    implemented_methods = Controls::Subject.implemented_methods
-
-    implemented_methods.each do |m|
-      context "#{m}" do
-        result = mimic.__send__(m)
-
-        test "Voided" do
-          assert(result.instance_of?(Void))
-        end
-      end
-    end
-  end
 end
