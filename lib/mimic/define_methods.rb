59a6b2c23bbfa2a8f9c5febc97255b669105a27e
Scott Bellware <sbellware@gmail.com>
2019-08-15 13:48:52 -0500

    Generate method

--- lib/mimic/define_methods.rb
+++ lib/mimic/define_methods.rb
@@ -11,6 +11,90 @@ module Mimic
     end
 
     def self.define_method(cls, method_name, record)
+      cls.class_eval(method_definition(cls, method_name, record))
+    end
+
+    def self.method_definition(cls, method_name, record)
+      # cls.define_method(method_name) do |*|
+      #   if record
+      #     invocation = Invocation.build(binding)
+      #     __record(invocation)
+      #   end
+
+      #   Void.new
+      # end
+
+      definition =
+        signature(cls, method_name) +
+        body(record) +
+        'end'
+
+      definition
+    end
+
+    def self.signature(cls, method_name)
+
+
+cls.instance_methods.sort.each do |m|
+  puts m
+end
+
+pp cls
+
+
+      mthd = cls.instance_method(method_name)
+
+      parameter_list = parameter_list(mthd.parameters)
+
+      sig = ''
+      sig
+    end
+
+    def self.parameter_list(parameters)
+      parameter_list = ''
+      parameters.each do |parameter|
+        parameter_list << "#{parameter_signature(*parameter)}, "
+      end
+
+      parameter_list[0...-2]
+    end
+
+    def self.parameter_signature(type, name)
+      case type
+      when :req
+        return "#{name}, "
+      end
+    end
+
+    def self.body(record)
+      body = "\n"
+
+      if record
+        body << <<~RECORD
+          invocation = Invocation.build(binding)
+          __record(invocation)
+        RECORD
+      end
+
+      body << "Void.new\n"
+      body
+    end
+  end
+end
+
+__END__
+
+target_class.class_eval(initializer_definition)
+
+def initializer_definition
+  "
+    def initialize(#{parameter_list})
+      #{variable_assignment_statements}
+    end
+  "
+end
+
+    def self.___define_method(cls, method_name, record)
       cls.define_method(method_name) do |*|
         if record
           invocation = Invocation.build(binding)
@@ -20,5 +104,3 @@ module Mimic
         Void.new
       end
     end
-  end
-end
