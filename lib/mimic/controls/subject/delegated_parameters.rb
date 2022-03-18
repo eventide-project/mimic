5cd5e6f58a827649d497bd3abd27c9ae9724214d
Shaun Dern <shaun@substantial.com>
2022-03-18 14:06:07 +0000

    Ruby 2.7 delegated parameters compatibility

---
+++ lib/mimic/controls/subject/delegated_parameters.rb
@@ -0,0 +1,12 @@
+module Mimic
+  module Controls
+    module Subject
+      module DelegatedParameters
+        class Example
+          def some_method(...)
+          end
+        end
+      end
+    end
+  end
+end
--- lib/mimic/predicates/method/parameters.rb
+++ lib/mimic/predicates/method/parameters.rb
@@ -22,6 +22,30 @@ module Mimic
 
           compare_parameters == control_parameters
         end
+
+        def self.types_equal?(compare, control, method_name)
+          control_method = control.method(method_name)
+
+          control_parameters = control_method.parameters
+          control_parameter_types = control_parameters.map(&:first)
+
+          compare_method = compare.method(method_name)
+
+          compare_parameters = compare_method.parameters
+          compare_parameter_types = compare_parameters.map(&:first)
+
+          if ENV['VERBOSE'] == 'on'
+            puts "Control: #{control.class}"
+            pp control_parameter_types
+
+            puts
+
+            puts "Compare: #{compare.class}"
+            pp compare_parameter_types
+          end
+
+          compare_parameter_types == control_parameter_types
+        end
       end
     end
   end
