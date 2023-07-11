3c8155978ff22a07dd6b0137c8dd80e2d14fa55d
Scott Bellware <sbellware@gmail.com>
2023-07-11 15:59:09 -0500

    Inverted test names are corrected

--- test/automated/arity/no_parameters.rb
+++ test/automated/arity/no_parameters.rb
@@ -2,14 +2,14 @@ require_relative '../automated_init'
 
 context "Mimic" do
   context "Arity" do
-    context "Positional Parameters Received by Mimic" do
-      subject_class = Controls::Subject::PositionalParameters::Example
+    context "No Parameters Received by Mimic" do
+      subject_class = Controls::Subject::NoParameters::Example
 
       mimic = Mimic.(subject_class)
 
       test "Not an error" do
         refute_raises(ArgumentError) do
-          mimic.some_method(1, 11)
+          mimic.some_method
         end
       end
     end
--- test/automated/arity/positional_parameters.rb
+++ test/automated/arity/positional_parameters.rb
@@ -2,14 +2,14 @@ require_relative '../automated_init'
 
 context "Mimic" do
   context "Arity" do
-    context "No Parameters Received by Mimic" do
-      subject_class = Controls::Subject::NoParameters::Example
+    context "Positional Parameters Received by Mimic" do
+      subject_class = Controls::Subject::PositionalParameters::Example
 
       mimic = Mimic.(subject_class)
 
       test "Not an error" do
         refute_raises(ArgumentError) do
-          mimic.some_method
+          mimic.some_method(1, 11)
         end
       end
     end
